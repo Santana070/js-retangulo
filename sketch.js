85b2fc9f0b48f1e2f56c51fba846708ba53d2634
Santana070 <joaosantana0310@gmail.com>
2024-06-03 08:17:06 -0300

    atz

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(500, 500);
+     background("black");
+  }
+  
+  function draw() {
+  
+    
+   stroke("red") 
+  fill("white")
+    
+  
+    if(mouseIsPressed) {
+    //console.log(mouseIsPressed);
+    rect(mouseX,mouseY,20,35);  
+   }
+  }
